eab58858c84a66abdd29a36b7abe33fac06c76bf
lascari <lascari565758@gmail.com>
2022-06-11 17:35:32 +0300

    push

--- src/services/cars/cars.hooks.js
+++ src/services/cars/cars.hooks.js
@@ -1,5 +1,7 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
 
+const createModel = require('../../models/cars.model');
+
 module.exports = {
   before: {
     // all: [ authenticate('jwt') ],
@@ -13,7 +15,41 @@ module.exports = {
 
   after: {
     all: [],
-    find: [],
+    find: [
+      async context => {
+        if (context?.params?.query?.full) {
+          let filters = {
+            make: {},
+            model: {},
+            series: {},
+            odometer: {},
+            location: {},
+            status: {},
+            keys: {},
+            fuel: {},
+            drive: {},
+            engine: {},
+            document: {},
+
+          };
+
+          const model = createModel(context.app);
+
+          const data = await model.find();
+
+          for (let item of data) {
+            for (let key of Object.keys(filters)) {
+              if (filters[key][item[key]]) filters[key][item[key]] += 1;
+              else filters[key][item[key]] = 1;
+            }
+          }
+
+          context.result.filters = filters;
+        }
+
+        return context;
+      }
+    ],
     get: [],
     create: [],
     update: [],
--- src/services/cars/cars.service.js
+++ src/services/cars/cars.service.js
@@ -1,5 +1,6 @@
 // Initializes the `cars` service on path `/cars`
 const { Cars } = require('./cars.class');
+const { CarsRefresh } = require('./cars-refresh.class');
 const createModel = require('../../models/cars.model');
 const hooks = require('./cars.hooks');
 
@@ -11,6 +12,8 @@ module.exports = function (app) {
 
   // Initialize our service with any options it requires
   app.use('/cars', new Cars(options, app));
+
+  app.use('/cars-refresh', new CarsRefresh(options, app));
 
   // Get our initialized service so that we can register hooks
   const service = app.service('cars');
