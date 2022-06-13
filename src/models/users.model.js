f5fa7f883c296c3a289bebdbc93da66079a0f049
lascari <lascari565758@gmail.com>
2022-06-13 22:29:26 +0300

    fixs

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 // users-model.js - A mongoose model
 //
 // See http://mongoosejs.com/docs/models.html
@@ -6,14 +8,43 @@ module.exports = function (app) {
   const modelName = 'users';
   const mongooseClient = app.get('mongooseClient');
   const schema = new mongooseClient.Schema({
-  
-    email: { type: String, unique: true, lowercase: true },
-    password: { type: String },
-  
-  
+    email: {type: String, required: true, unique: true},
+    password: {type: String},
+    username: {type: String},
+    phone: {type: String},
+    balance: {type: Number, default: 0},
+    status: {type: Boolean, default: 1},
+    group: {type: String, default: "new"},
+    role: {type: String, default: "user"},
+    price: {type: JSON, default: {carfax: null, autocheck: null, sticker: null}},
+    token: {type: String},
+    domain: {type: String},
+    g_uid: {type: String},
+    g_name: {type: String},
+    registration_date: {type: Date, default: Date.now},
+    ip: {type: String},
+    country: {type: String},
+    rep: {type: JSON},
+    comment: {type: String},
+    // reports: [{ type: mongooseClient.Types.ObjectId, ref: 'Report' }],
+    // billings: [{ type: mongooseClient.Types.ObjectId, ref: 'Billing' }],
+    resetPasswordToken: {
+        type: String,
+        required: false
+    },
+
+    resetPasswordExpires: {
+        type: Date,
+        required: false
+    }
   }, {
     timestamps: true
   });
+
+  schema.methods.generatePasswordReset = function() {
+    this.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+    this.resetPasswordExpires = Date.now() + 3600000; //expires in an hour
+  };
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
