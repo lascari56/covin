573efad41163bfd0f22db538b49a8e8a571a52f2
lascari <lascari565758@gmail.com>
2022-06-15 00:55:00 +0300

    search

--- src/services/cars/cars.service.js
+++ src/services/cars/cars.service.js
@@ -7,7 +7,8 @@ const hooks = require('./cars.hooks');
 module.exports = function (app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: app.get('paginate'),
+    whitelist: ['$text','$search', '$regex']
   };
 
   // Initialize our service with any options it requires
