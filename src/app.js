5f88216abb36db0d0f2c101642e36a909b5ed787
lascari <lascari565758@gmail.com>
2022-07-07 01:56:20 +0300

    ppush

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 const logger = require('./logger');
 
+const AWS = require('aws-sdk');
+const Store = require('s3-blob-store');
+const BlobService = require('feathers-blob');
+
 const feathers = require('@feathersjs/feathers');
 const configuration = require('@feathersjs/configuration');
 const express = require('@feathersjs/express');
@@ -22,6 +26,30 @@ const mongoose = require('./mongoose');
 
 const app = express(feathers());
 
+const s3 = new AWS.S3({
+  region: 'ams3',
+  endpoint: 'https://ams3.digitaloceanspaces.com',
+  accessKeyId: 'FIVVXMS363NGP3CS4X67',
+  secretAccessKey: 'XATO3zBr/uJnYp65BzllKolmcV3Gyh5ofVmpDbm4ylg',
+  // ACL: 'public-read-write'
+});
+
+// let spaces = await s3.listBuckets();
+
+// console.log("spaces", spaces);
+
+const blobStore = Store({
+  client: s3,
+  bucket: 'unocreative',
+  acl: 'public-read',
+  key: "covin"
+});
+
+const blobService = BlobService({
+  Model: blobStore,
+  // ACL: 'public-read-write'
+});
+
 // Load app configuration
 app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
@@ -35,6 +63,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
+app.use('/upload', blobService);
+
+// app.service('upload').before({
+//   create(hook) {
+//     hook.params.s3 = { ACL: 'public-read' }; // makes uploaded files public
+//   }
+// });
 
 // Set up Plugins and providers
 app.configure(express.rest());
